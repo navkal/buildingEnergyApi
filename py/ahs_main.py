0808add1ea350bed94d0a9aa0300c89e5fecf708
navkal <navkal@hotmail.com>
2018-11-01 23:27:47 -0400

    Primitive coding example

---
+++ py/ahs_main.py
@@ -0,0 +1,9 @@
+# Copyright 2018 BACnet Gateway.  All rights reserved.
+
+from building_data_requests import get_value
+value, units = get_value( 'ahs', 3007360 )
+print( 'AHS Main: {0} {1}'.format( int( value ), units ) )
+
+temp_value, temp_units = get_value( 'ahs', 3001489 )
+co2_value, co2_units = get_value( 'ahs', 3001477 )
+print( 'AHS 351: {0} {1} | CO2 {2} {3}'.format( int( temp_value ), temp_units, int( co2_value ), co2_units ) )
