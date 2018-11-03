cdeb8254a83784907172d36991569f0741fbf089
navkal <navkal@hotmail.com>
2018-11-02 23:01:39 -0400

    Coding examples

--- py/ahs_power.py
+++ py/ahs_power.py
@@ -1,41 +1,34 @@
 # Copyright 2018 BACnet Gateway.  All rights reserved.
 
-try:
-    import time
-    import argparse
-    import pandas as pd
-    from building_data_requests import get_value
-    import numbers
+import time
+import argparse
+import pandas as pd
+from building_data_requests import get_value
+import numbers
 
-    start_time = time.time()
+start_time = time.time()
 
-    # Read spreadsheet into a dataframe.
-    # Each row contains the following:
-    #   - Feeder
-    #   - Instance ID of electric meter
-    df = pd.read_csv( '../csv/ahs_power.csv' )
+# Read spreadsheet into a dataframe.
+# Each row contains the following:
+#   - Feeder
+#   - Instance ID of electric meter
+df = pd.read_csv( '../csv/ahs_power.csv' )
 
-    # Output column headings
-    print( 'Feeder,Meter,Units' )
+# Output column headings
+print( 'Feeder,Meter,Units' )
 
-    # Iterate over the rows of the dataframe, getting meter readings for each feeder
-    for index, row in df.iterrows():
-        # Retrieve data
-        value, units = get_value( row['Facility'], row['Meter'] )
+# Iterate over the rows of the dataframe, getting meter readings for each feeder
+for index, row in df.iterrows():
+    # Retrieve data
+    value, units = get_value( row['Facility'], row['Meter'] )
 
-        # Prepare to print
-        value = int( value ) if isinstance( value, numbers.Number ) else ''
-        units = units if units else ''
+    # Prepare to print
+    value = int( value ) if isinstance( value, numbers.Number ) else ''
+    units = units if units else ''
 
-        # Output CSV format
-        print( '{0},{1},{2}'.format( row['Label'], value, units ) )
+    # Output CSV format
+    print( '{0},{1},{2}'.format( row['Label'], value, units ) )
 
-    # Report elapsed time
-    elapsed_time = round( ( time.time() - start_time ) * 1000 ) / 1000
-    print( '\nElapsed time: {0} seconds'.format( elapsed_time ) )
-
-
-except KeyboardInterrupt:
-    print( 'Bye' )
-    import sys
-    sys.exit()
+# Report elapsed time
+elapsed_time = round( ( time.time() - start_time ) * 1000 ) / 1000
+print( '\nElapsed time: {0} seconds'.format( elapsed_time ) )
